d8b54b810b4b5e7f3cedc3d62774dcea870004c6
dev-brz <bartlomiejrzepka@wp.pl>
2025-11-30 12:52:44 +0100

    chore: set up angular 21 project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,12 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
+/*
+        // "@stomp/stompjs": "^7.2.0",
+        // "sockjs-client": "^1.6.1"
+        // "@types/sockjs-client": "^1.5.4",
+*/
 }
