63ba7f05480a8bfbad2d8d8b008e2475d9d92353
dev-brz <bartlomiejrzepka@wp.pl>
2025-11-30 13:31:34 +0100

    feature: set up initial websocket connection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
+import { RxStomp } from '@stomp/rx-stomp';
+import { switchMap, timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,10 +8,32 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrl: './app.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent {
-/*
-        // "@stomp/stompjs": "^7.2.0",
-        // "sockjs-client": "^1.6.1"
-        // "@types/sockjs-client": "^1.5.4",
-*/
+export class AppComponent implements OnInit, OnDestroy {
+  rxStomp = new RxStomp();
+
+  ngOnInit(): void {
+    this.rxStomp.configure({
+      brokerURL: 'ws://localhost:8080/ws',
+    });
+    
+    this.rxStomp
+      .watch({ destination: "/topic/chat/broadcast" })
+      .subscribe((message) => console.log(`Received ${message.body}`));
+
+    this.rxStomp.connected$
+      .pipe(switchMap(() => timer(3000)))
+      .subscribe(() => {
+        const frame = {
+          destination: '/app/chat',
+          body: JSON.stringify({ content: 'hello world' }),
+        };
+        this.rxStomp.publish(frame);
+      });
+
+    this.rxStomp.activate();
+  }
+
+  ngOnDestroy(): void {
+    this.rxStomp.deactivate();
+  }
 }
