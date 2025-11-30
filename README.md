0c009d40d45c6739a429017038207d508ca1ba3d
dev-brz <bartlomiejrzepka@wp.pl>
2025-11-30 12:52:44 +0100

    chore: set up angular 21 project

---
+++ README.md
@@ -0,0 +1,6 @@
+# Stack
+- Angular 21
+    - zoneless approach
+    - vitest (with jsdom) instead of karma & jasmine
+- Eslint (for Angular)
+- Stomp & SockJS for websockets
---
+++ eslint.config.mjs
@@ -0,0 +1,50 @@
+import { defineConfig } from "eslint/config";
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import angular from 'angular-eslint';
+import stylistic from '@stylistic/eslint-plugin'
+
+export default defineConfig([
+  {
+    files: ["**/*.ts"],
+    extends: [
+      eslint.configs.recommended,
+      tseslint.configs.recommended,
+      tseslint.configs.stylistic,
+      angular.configs.tsRecommended,
+      stylistic.configs.recommended
+    ],
+    plugins: { 
+       '@stylistic': stylistic
+    },
+    processor: angular.processInlineTemplates,
+    rules: {
+      "@angular-eslint/directive-selector": [
+        "error",
+        {
+          type: "attribute",
+          prefix: "app",
+          style: "camelCase",
+        },
+      ],
+      "@angular-eslint/component-selector": [
+        "error",
+        {
+          type: "element",
+          prefix: "app",
+          style: "kebab-case",
+        },
+      ],
+      "@stylistic/semi": ['error', 'always'],
+      "@stylistic/brace-style": ['warn', '1tbs']
+    },
+  },
+  {
+    files: ["**/*.html"],
+    extends: [
+      angular.configs.templateRecommended,
+      angular.configs.templateAccessibility,
+    ],
+    rules: {},
+  },
+]);
